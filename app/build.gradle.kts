c843075e1cc644dd64eb7f003d217d4f986ac07c
Wc man <Quertical5@gmail.com>
2023-12-01 14:20:20 +0100

    Final commit (App is finished)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.greetingsapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.greetingsapp"
--- app/src/main/java/com/example/greetingsapp/MainActivity.kt
+++ app/src/main/java/com/example/greetingsapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.greetingsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv: TextView = findViewById(R.id.tv1)
+        val et: EditText = findViewById(R.id.et1)
+        val btn = findViewById<Button>(R.id.btn1)
+
+        btn.setOnClickListener(){
+            val userName: String = et.text.toString()
+            Toast.makeText(this, "Hello $userName", Toast.LENGTH_SHORT).show()
+        }
     }
 }
